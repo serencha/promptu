f57757e90cfbc4119b7d54d23c294c094594d655
Jesse Lee <jesselee@Jesses-MacBook-Air-3.local>
2022-10-16 00:12:33 -0400

    enabled cors, restructured api res

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,54 +1,69 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
-const FieldValue = require('firebase-admin').firestore.FieldValue;
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-	functions.logger.info('Hello logs!', { structuredData: true });
-	response.send('Hello from Firebase!');
-});
+const FieldValue = require("firebase-admin").firestore.FieldValue;
+const cors = require("cors")({ origin: "*" });
 
 exports.getUsers = functions.https.onRequest(async (request, response) => {
-	const users = await db.collection('users').get();
-	response.send(users.docs.map((doc) => doc.data()));
+  cors(request, response, async () => {
+    const users = await db.collection("users").get();
+    response.send(
+      users.docs.map((doc) => {
+        return { data: doc.data(), id: doc.id };
+      })
+    );
+  });
 });
 
 exports.updateLikes = functions.https.onRequest(async (request, response) => {
-	const id = request.query.postId;
-	await db
-		.collection('posts')
-		.doc(id)
-		.update({ likes: FieldValue.increment(1) });
+  cors(request, response, async () => {
+    const id = request.query.postId;
+    await db
+      .collection("posts")
+      .doc(id)
+      .update({ likes: FieldValue.increment(1) });
 
-	const newPost = await db.collection('posts').doc(id).get();
-	response.send({ likes: newPost.data().likes }); // lmao unoptimal but works for now
+    const newPost = await db.collection("posts").doc(id).get();
+    response.send({ likes: newPost.data().likes, id: newPost.id }); // lmao unoptimal but works for now
+  });
 });
 
-// todo -> format properly
-exports.getPrompts = functions.https.onRequest(async (request, response) => {
-	const today = new Date(); // wrong format, figure out then we chilling
-	console.log(today);
-	const prompts = await db
-		.collection('prompts', (ref) => ref)
-		.where('end_time', '>=', today) // bruh why dont let u do two where statements
-		.get();
-
-	response.send(
-		prompts.docs
-			.map((doc) => doc.data())
-			.filter((d) => {
-				return d.start_time.toMillis() <= today.getTime();
-			})
-	);
+exports.getPrompts = functions.https.onRequest(async (req, res) => {
+  cors(req, res, async () => {
+    const today = new Date(); // wrong format, figure out then we chilling
+    const prompts = await db
+      .collection("prompts", (ref) => ref)
+      .where("end_time", ">=", today) // bruh why dont let u do two where statements
+      .get();
+    res.send(
+      prompts.docs
+        .map((doc) => [doc.data(), doc.id])
+        .filter((d) => {
+          return d[0].start_time.toMillis() <= today.getTime();
+        })
+        .map((d2) => {
+          return {
+            description: d2[0].description,
+            id: d2[1],
+          };
+        })
+    );
+  });
 });
 
 exports.getPosts = functions.https.onRequest(async (request, response) => {
-	const posts = await db.collection('posts').get();
-	response.send(posts.docs.map((doc) => doc.data()));
+  cors(request, response, async () => {
+    const posts = await db.collection("posts").get();
+    response.send(
+      posts.docs.map((doc) => {
+        return {
+          id: doc.id,
+          data: doc.data(),
+        };
+      })
+    );
+  });
 });
 
 // exports.getLikes =
